ac9c0c8f5d24d47d663ac16f52e559cdbe9568c2
Shiloh Yang <77713004+YXR9@users.noreply.github.com>
2022-10-14 12:15:55 +0800

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,13 +1,17 @@
 var mysql = require('mysql');
+var host = process.env.DB_HOST_URL || "localhost"
+var user = process.env.DB_USER_ACCOUNT || "root"
+var password = process.env.DB_USER_PASSWORD || "root"
+var database = process.env.MYSQL_DATABASE || "adminPage"
 
 var pool  = mysql.createPool({
-    user: 'root',
-    password: '',
+    user: user,
+    password: password,
     port: 3306,
-    host: 'localhost',
-    database: 'knowledge forum', 
+    host: host,
+    database: database, 
     waitForConnections : true, 
     connectionLimit : 1000       
 });
-0
+
 module.exports = pool;
