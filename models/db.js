e537e60fdaa9ebbb7ecc79675e628d8d7859efd0
YXR9 <yangxuru870918@example.com>
2023-03-02 15:16:15 +0800

    build image

--- models/db.js
+++ models/db.js
@@ -1,8 +1,8 @@
 var mysql = require('mysql');
-var host = process.env.DB_HOST_URL || "localhost"
-var user = process.env.DB_USER_ACCOUNT || "root"
-var password = process.env.DB_USER_PASSWORD || "root"
-var database = process.env.MYSQL_DATABASE || "adminPage"
+var host = process.env.DB_HOST_URL || "140.115.126.19"
+var user = process.env.DB_USER_ACCOUNT || "wulab"
+var password = process.env.DB_USER_PASSWORD || "ytwu35415"
+var database = process.env.MYSQL_DATABASE || "inquiry"
 
 var pool  = mysql.createPool({
     user: user,
