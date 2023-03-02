a756793c1bc2a7b5fd1c92f5da819a2c82a504e0
YXR <yangxuru870918@gmail.com>
2023-03-02 11:39:15 +0800

    Up to Lab server.- DB

--- models/db.js
+++ models/db.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql');
 
 var pool  = mysql.createPool({
-    user: 'root',
-    password: '',
+    user: 'wulab',
+    password: 'ytwu35415',
     port: 3306,
-    host: 'localhost',
-    database: 'knowledge forum', 
+    host: '140.115.126.19',
+    database: 'inquiry', 
     waitForConnections : true, 
     connectionLimit : 1000       
 });
